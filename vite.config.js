233067a7a85e71863714ffd7ec762b94a535f8ef
Mukhlisa <mukhlisanematova>
2026-02-08 14:49:12 -0500

    fixing deployment

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/',
+  base: '/mukhlisabonunematova.github.io/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, './src')
