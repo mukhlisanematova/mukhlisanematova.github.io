991137bed21610e892cfd4c87066d77e60f88a26
mukhlisa <mnematova24@gmail.com>
2026-04-11 22:59:58 -0400

    fixed descriptions and details!

--- src/sections/News.jsx
+++ src/sections/News.jsx
@@ -1,14 +1,16 @@
 export const News = () => {
     const newsItems = [
-        { date: "March 2026", dateTime: "2025-09", text: "Paper 'Datum Fieldnotes: Easing the Data-Work Burden on Civic and Non-Profit Organizations.' accepted at the ACM CHI Conference on Human Factors in Computing Systems (CHIWORK ‘26), San Francisco, CA, 2026' "},
-        { date: "February 2026", dateTime: "2026-02", text: "Paper 'We’re in This Together (WIT): Interaction and Data Representation in Collaborative Mixed Reality Agent-Based Participatory Simulations of Complex Phenomena.' accepted at the ACM International Conference of the Learning Sciences (ISLS ‘26), Singapore, 2026" },
+        { date: "March 2026", dateTime: "2025-09", text: <>Paper ‘<em>'Datum Fieldnotes: Learning How Civic and Non-Profit Data Workers Perform Data Contextualization In Situ'</em> <strong>accepted</strong> at the ACM CHI Conference on Human Factors in Computing Systems (CHIWORK ‘26), San Francisco, CA, 2026'</>},
+        { date: "February 2026", dateTime: "2026-02", text:  <>Paper ‘<em>'We’re in This Together (WIT): Interaction and Data Representation in Collaborative Mixed Reality Agent-Based Participatory Simulations of Complex Phenomena.'</em> <strong> accepted </strong> at the ACM International Conference of the Learning Sciences (ISLS ‘26), Singapore, 2026'</> },
         { date: "January 2026", dateTime: "2026-01", text: "Graduated with my Bachelors of Arts in Computer Science from Wellesley College." },        
         { date: "June 2025", dateTime: "2025-07", text: "Joined the MIT Scheller Teacher Education Lab as an XR engineer!" },
         { date: "April 2025", dateTime: "2025-04", text: "Presented REU and paper on Datum Fieldnotes at Wellesley's 29th Ruhlman Conference."},
-        { date: "March 2025", dateTime: "2025-03", text: "Paper 'Datum Fieldnotes: Easing the Data-Work Burden on Civic and Non-Profit Organizations.' accepted at the ACM CHI Conference on Human Factors in Computing Systems (CHI ‘25), Yokohama, Japan, 2025' "},
+        { date: "March 2025", dateTime: "2025-03", text: <>Paper ‘<em>Datum Fieldnotes: Easing the Data-Work Burden on Civic and Non-Profit Organizations.</em>’ <strong>accepted</strong> at the ACM CHI Conference on Human Factors in Computing Systems (CHI ‘25), Yokohama, Japan, 2025’</> },
         { date: "June 2024", dateTime: "2024-06", text: "Started REU at Georgia Tech's Data Works, under mentorship of Professor Betsy DiSalvo and Annabel Rothschild!" },
         { date: "August 2023", dateTime: "2023-08", text: "Transferred and started first semester as a Junior at Wellesley College." },
-        { date: "June 2023", dateTime: "2023-06", text: "Graduated with my Associate of Science (A.S) in Computer Science from Borough of Manhattan Community College." },
+        { date: "June 2023", dateTime: "2023-06", text: "Graduated with my Associate of Science (A.S) in Computer Science (Honors) from Borough of Manhattan Community College." },
+        { date: "March 2023", dateTime: "", text: "Accepted as semi-finalist for Jack Kent Cooke Undergraduate Transfer Scholarship, awarded to high-achieving community college students transferring to 4-year institutions." },
+
     ];
 
     return (
